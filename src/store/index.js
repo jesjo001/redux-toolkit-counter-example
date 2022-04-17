ac2c8ffd152d2790ec76146f1495e5a0d81d3fce
jesjo001 <automationlounge@gmail.com>
2022-04-17 21:55:07 +0100

    login implemented

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from 'redux'
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 import { configure } from '@testing-library/react';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers:{
         increment(state) {
             state.counter++;
@@ -23,10 +23,28 @@ const counterSlice = createSlice({
     }
 })
 
+const initialAuthState = { isAuthenticated: false }
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        login(state){
+            state.isAuthenticated = true;
+        },
+        logout(state){
+            state.isAuthenticated = false;
+        }
+    }
+})
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
 });
 
+export const authActions = authSlice.actions;
 export const counterActions = counterSlice.actions;
 export default store;
